2a3adcad4fc3f67faa1c5c64f276081c1f0bffb1
Justin Urbany <justin.urbany@oit.edu>
2018-10-17 17:02:48 -0700

    Added first Model

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASPTermProject.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASPTermProject.Controllers
 {
     public class ContactController : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        public IActionResult Index(ContactModel contact)
         {
-            return View();
+            return View(contact);
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Create(ContactModel contact)
+        {
+            return View(contact);
         }
     }
 }
---
+++ Models/ContactModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPTermProject.Models
+{
+    public class ContactModel
+    {
+        [Display(Name ="Name")]
+        [Required(ErrorMessage = "Name is Required")]
+        public string Name { get; set; }
+        [Display(Name ="PhoneNumber")]
+        [Required(ErrorMessage = "PhoneNumber is Required")]
+        public string PhoneNumber { get; set; }
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Email is Required")]
+        public string Email { get; set; }
+        [Display(Name = "Message")]
+        [Required(ErrorMessage = "Message is Required")]
+        public string Message { get; set; }
+    }
+}
