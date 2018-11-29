a18159ed115532cd5ffae985c0321d7373bb14f1
Justin Urbany <justin.urbany@labs.cset.oit.edu>
2018-11-29 15:14:04 -0800

    its going

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASPTermProject.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASPTermProject.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DogSearch(DogModel model)
         {
             return View();
         }
---
+++ Models/DogModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPTermProject.Models
+{
+    public class DogModel
+    {
+        public string DogType { get; set; }
+        public int Age { get; set; }
+        public string Sex { get; set; }
+
+        public static readonly List<string> DogTypes = new List<string> { "German Sheppard", "Bull Dog", "Labrador Retriever" };
+    }
+}
